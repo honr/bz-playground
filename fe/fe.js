d4daca875b260ef669f8fca89009cfcb3ebb152d
El Rey <el@rey>
2016-06-30 09:04:19 -0700

    minor

--- fe/fe.js
+++ fe/fe.js
@@ -3,18 +3,39 @@ import 'goog:goog.soy';
 import 'goog:fe.soy_main';
 
 export class Greeter {
-  /** @param{string} name**/
+  /** @param{string} name */
   constructor(name) {
     this.name_ = name;
   }
 
   /****/
   greet() {
-    goog.soy.renderElement(goog.global.document.body,
-                           fe.soy_main.greet,
-                           {name: this.name_});
+    console.log('this.name_: ' + this.name_);
+    return goog.soy.renderElement(
+      goog.global.document.body, fe.soy_main.greet, {
+        name: this.name_,
+      });
+  }
+
+  /****/
+  greetWorld() {
+    return goog.soy.renderElement(
+      goog.global.document.body, fe.soy_main.greet, {
+        name: 'World',
+      });
+  }
+
+  /****/
+  run() {
+    // this.greet();
+    let g1 = this.greet.bind(this);
+    let g2 = this.greetWorld.bind(this);
+    for (let x of [0, 1, 2, 3, 4, 5]) {
+      let t = x * 1000;
+      let f = (x % 2 === 0) ? g1 : g2;
+      window.setTimeout(f, t);
+    }
   }
 }
 
-export var g = new Greeter('Justine');
-g.greet();
+new Greeter('Justine').run();
