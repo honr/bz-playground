fb717ad46e74c36f27694e34654534cef36b91fc
El Rey <el@rey>
2016-07-02 11:51:17 -0700

    minor

---
+++ fe/r/greeter.js
@@ -0,0 +1,44 @@
+import 'goog:goog.soy';
+
+import 'goog:r.greeter_soy';
+
+export class Greeter {
+  /** @param{string} name */
+  constructor(name, numMarbles) {
+    this.name_ = name;
+  }
+
+  /** @param{number} n
+   *  @param{Array<string>} xs */
+  greet(n, xs) {
+    return goog.soy.renderElement(
+      goog.global.document.body, r.greeter_soy.greet, {
+        name: this.name_,
+        numMarbles: n,
+        xs: xs || [],
+      });
+  }
+
+  /** @param{number} n */
+  greetWorld(n) {
+    return goog.soy.renderElement(
+      goog.global.document.body, r.greeter_soy.greet, {
+        name: 'World',
+        numMarbles: n,
+        xs: [],
+      });
+  }
+
+  /****/
+  run() {
+    let xs = ['foo', 'bar', 'baz'];
+    for (let x of [0, 1, 2, 3, 4, 5]) {
+      let t = x * 1000;
+      if (x % 2 === 0) {
+        window.setTimeout(() => this.greet(x, xs), t);
+      } else {
+        window.setTimeout(() => this.greetWorld(x), t);
+      }
+    }
+  }
+}
